751982dfe27f3d1918f008e7e7117f36b966a344
Christophe HENRY <christophe.henry@sbgodin.fr>
2014-06-24 20:28:34 +0200

    Corrige "Agregator" en "Aggregator" (anglais)

--- Feed.class.php
+++ Feed.class.php
@@ -79,6 +79,7 @@ class Feed extends MysqlEntity{
         $feed->force_feed($forceFeed);
         $feed->set_feed_url($this->url);
         $feed->set_useragent('Mozilla/4.0 Leed (LightFeed Aggregator) '.VERSION_NAME.' by idleman http://projet.idleman.fr/leed');
+        $this->lastSyncInError = 0;
         if (!$feed->init()) {
             $this->error = $feed->error;
             $this->lastSyncInError = 1;
